687dfabb851cec435e9a5cbee5f1f774604e8976
math-mmp <matheusmp2001@gmail.com>
2021-06-19 13:19:20 -0300

    step 3 completed, step 4 started

--- QRCode.py
+++ QRCode.py
@@ -6,22 +6,49 @@ from dataTables import characterCountIndicators as cCIs
 from dataTables import errorCorrectionCodeWordsBlockInformation as eCCWBI
 
 class QRCode:
-  modeIndicator = None
-  characterCountIndicatorSize = None
-  characterCountIndicator = None
-  encodedData = None
-  bitString = None
-
   def __init__(self, text, errorCorretionLevel = 'H'):
     self.text = text
-    self.textLength = len(text)
-    self.encodingMode = None
     self.errorCorretionLevel = errorCorretionLevel
+    self.encodingMode = None
     self.version = None
-  
-  def create(self):
+    self.__logAntilog = []
+    self.__generatorPolynomial = []
+    self.__messagePolynomial = []
+    self.__errorCorrectionCodewords = []
 
-    # Step 1: Data Analysis 
+  def __integerNotation2AlphaNotation(self, integerNotation):
+    alphaNotation = [self.__logAntilog.index(i) for i in integerNotation]
+    return alphaNotation
+  
+  def __alphaNotation2IntegerNotation(self, alphaNotation):
+    integerNotation = [self.__logAntilog[i] for i in alphaNotation]
+    return integerNotation
+
+  def __greaterThanOrEqualTo256(self, array):
+    for i in range(len(array)):
+      array[i] = array[i]%255 if array[i] >= 256 else array[i]
+    return array
+  
+  def __generateErrorCorrectionCodewords(self, block):
+    for steps in range(len(block)):
+      generatorPolynomialMultiplied = self.__generatorPolynomial.copy()
+      leadTerm = self.__integerNotation2AlphaNotation([block[-1]])
+      for i in range(len(self.__generatorPolynomial)):
+        generatorPolynomialMultiplied[i] += leadTerm[-1]
+      generatorPolynomialMultiplied = self.__greaterThanOrEqualTo256(generatorPolynomialMultiplied)
+      generatorPolynomialMultiplied = self.__alphaNotation2IntegerNotation(generatorPolynomialMultiplied)
+
+      if len(block) > len(self.__generatorPolynomial):
+        generatorPolynomialMultiplied = [0]*(len(block) - len(self.__generatorPolynomial)) + generatorPolynomialMultiplied
+        block = [i^j if j!=0 else i for i, j in zip(block, generatorPolynomialMultiplied)]
+      else:
+        block =  [0]*(len(self.__generatorPolynomial) - len(block)) + block
+        block = [i^j if j!=0 else i for i, j in zip(generatorPolynomialMultiplied, block)]
+      block.pop()
+    return block[::-1]
+
+  def create(self):
+    ## Step 1: Data Analysis 
     if self.text.isnumeric():
       self.encodingMode = 'numeric'
     elif all(character in aVs for character in self.text):
@@ -36,7 +63,7 @@ class QRCode:
     for i in range(index, -1, -1): 
       data = cCs[self.encodingMode][i]
       try:
-        self.version = [data.index(maxLength) for maxLength in data if self.textLength <= maxLength][0] + 1
+        self.version = [data.index(maxLength) for maxLength in data if len(self.text) <= maxLength][0] + 1
         self.errorCorretionLevel = eCLs[i]
         break
       except:
@@ -52,18 +79,18 @@ class QRCode:
       characterCountIndicatorSize = cCIs[self.encodingMode][1]
     else:
       characterCountIndicatorSize = cCIs[self.encodingMode][2]
-    characterCountIndicator = '{:0{size}b}'.format(self.textLength, size = characterCountIndicatorSize)
+    characterCountIndicator = '{:0{size}b}'.format(len(self.text), size = characterCountIndicatorSize)
 
     # Step 2.5: Encode Using the Selected Mode
     groups = []
     if self.encodingMode == 'numeric':
-      for digits in range(0, self.textLength, 3):
+      for digits in range(0, len(self.text), 3):
         groups.append(int(self.text[digits:digits + 3]))
       binaryBits = (4, 7, 10)
       encodedData = ''.join('{:0{size}b}'.format(i, size = binaryBits[len(str(i))-1]) for i in groups)
     elif self.encodingMode == 'alphanumeric':
       pairs = []
-      for digits in range(0, self.textLength, 2):
+      for digits in range(0, len(self.text), 2):
          pairs.append(self.text[digits:digits + 2])
       groups = [(45* aVs.index(i[0])) + aVs.index(i[1]) if len(i) == 2 else aVs.index(i[0]) for i in pairs]
       binaryBits = (6, 11)
@@ -80,26 +107,58 @@ class QRCode:
       bitString += '0'
     padBytes = int((bitsRequired - len(bitString))/8)
     for i in range(padBytes):
-      if i%2 == 0:
-        bitString += '11101100'
-      else:
-        bitString += '00010001'
+      bitString += '11101100' if i%2 == 0 else '00010001'
 
-    # Step 3: Error Correction Coding
-    # Step 4: Structure Final Message
-    # Step 5: Module Placement in Matrix
-    # Step 6: Data Masking
-    # Step 7: Format and Version Information
+    ## Step 3: Error Correction Coding
+    # Step 3.5: Generate Powers of 2 Using Byte-Wise Modulo 100011101
+    self.__logAntilog = [1]
+    while len(self.__logAntilog) != 256:
+      i = self.__logAntilog[-1]*2
+      self.__logAntilog.append(i if i < 256 else i^285)
+    
+    # Step 3.7: Generator Polynomial
+    g = [1, 0]
+    self.__generatorPolynomial = [1, 25, 0]
+    for steps in range(eCCWBI[self.errorCorretionLevel][self.version - 1][1] - 2):
+      g[0] += 1 
+      auxGeneratorPolynomial = []
+      for i in range(len(self.__generatorPolynomial)):
+        auxGeneratorPolynomial.extend([self.__generatorPolynomial[i] + g[0], self.__generatorPolynomial[i] + g[1]])
+      auxGeneratorPolynomial = self.__greaterThanOrEqualTo256(auxGeneratorPolynomial)
+      auxGeneratorPolynomial = self.__alphaNotation2IntegerNotation(auxGeneratorPolynomial)
+      self.__generatorPolynomial = [auxGeneratorPolynomial[0]]
+      for i in range(1, len(auxGeneratorPolynomial)-1, 2):
+        self.__generatorPolynomial.append(auxGeneratorPolynomial[i]^auxGeneratorPolynomial[i+1])
+      self.__generatorPolynomial.append(auxGeneratorPolynomial[-1])
+      self.__generatorPolynomial = self.__integerNotation2AlphaNotation(self.__generatorPolynomial)
+
+    self.__messagePolynomial = [int(bitString[i:i+8], 2) for i in range(0,len(bitString),8)]
+
+    ## Step 4: Structure Final Message
+    # Step 4.1: Determine How Many Blocks and Error Correction Codewords are Required
+    auxMessagePolynomial = []
+    for i in range(2, 5, 2):
+      for j in range(0, eCCWBI[self.errorCorretionLevel][self.version - 1][i]):
+        blockSize = eCCWBI[self.errorCorretionLevel][self.version - 1][i+1]
+        auxMessagePolynomial.append(self.__messagePolynomial[blockSize*j:blockSize*(j+1)])
+        self.__errorCorrectionCodewords.append(self.__generateErrorCorrectionCodewords(auxMessagePolynomial[-1][::-1]))
+    self.__messagePolynomial = auxMessagePolynomial.copy()
+
+    print(self.__messagePolynomial)
+    print(self.__errorCorrectionCodewords)
+
+    ## Step 5: Module Placement in Matrix
+    ## Step 6: Data Masking
+    ## Step 7: Format and Version Information
 
   # def show(self):
 
   # def save(self):
 
-qrCode = QRCode('HELLO WORLD')
+qrCode = QRCode('UMA IMAGEM VALE MAIS DO QUE 1.000 PALAVRAS')
 qrCode.create()
 
-# Logs
 print('Tipo de Codificação:', qrCode.encodingMode)
-print('Tamanho do Texto:', qrCode.textLength)
+print('Tamanho do Texto:', len(qrCode.text))
 print('Nível de Correção:', qrCode.errorCorretionLevel)
 print('Versão:', qrCode.version)
--- README.md
+++ README.md
@@ -6,7 +6,7 @@ https://www.thonky.com/qr-code-tutorial/
 ## Steps
 - [x] Step 1: Data Analysis 
 - [x] Step 2: Data Encoding
-- [ ] Step 3: Error Correction Coding
+- [x] Step 3: Error Correction Coding
 - [ ] Step 4: Structure Final Message
 - [ ] Step 5: Module Placement in Matrix
 - [ ] Step 6: Data Masking
